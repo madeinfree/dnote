02014bf16d68354344d3b27cbacaa80a2e97207e
Whien <sal95610@gmail.com>
2019-10-23 17:46:51 +0800

    Init: first init

---
+++ precache-manifest.d8737bd82460a1f17fb51ed6eed5a718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f336eedcc253b3ca38b69c40314ab8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cdd9454a358fbb9284c",
+    "url": "/static/css/main.ade1ebea.chunk.css"
+  },
+  {
+    "revision": "954d457391985c75455b",
+    "url": "/static/js/2.9f9fafde.chunk.js"
+  },
+  {
+    "revision": "2cdd9454a358fbb9284c",
+    "url": "/static/js/main.c21cf569.chunk.js"
+  },
+  {
+    "revision": "7c18323746cf0dbb121f",
+    "url": "/static/js/runtime-main.928a287c.js"
+  },
+  {
+    "revision": "8abf742e58bdf986acd3c63721e33102",
+    "url": "/static/media/cover.8abf742e.png"
+  }
+]);
